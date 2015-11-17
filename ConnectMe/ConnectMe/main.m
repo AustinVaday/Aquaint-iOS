18788fc0121753958def905839c89e00fe725ad1
Austin Vaday <avaday0@ivc.edu>
2015-11-16 20:37:36 -0800

    Initialized repository for our new app. All the app code will be in the file labeled 'ConnectMe'. All other files are supplemental/assistive demonstrations.

---
+++ ConnectMe/ConnectMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ConnectMe
+//
+//  Created by Austin Vaday on 11/16/15.
+//  Copyright (c) 2015 ConnectMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Page-Based-Application-Demo/Page-Based-Application-Demo/ModelController.h
@@ -0,0 +1,18 @@
+//
+//  ModelController.h
+//  Page-Based-Application-Demo
+//
+//  Created by Austin Vaday on 11/16/15.
+//  Copyright (c) 2015 ConnectMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DataViewController;
+
+@interface ModelController : NSObject <UIPageViewControllerDataSource>
+
+- (DataViewController *)viewControllerAtIndex:(NSUInteger)index storyboard:(UIStoryboard *)storyboard;
+- (NSUInteger)indexOfViewController:(DataViewController *)viewController;
+
+@end
