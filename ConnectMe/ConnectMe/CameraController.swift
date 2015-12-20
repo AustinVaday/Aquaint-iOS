1efe288dcea2bb9b69f9dd48b894f4fdd540906f
Austin Vaday <avaday0@ivc.edu>
2015-12-20 17:08:00 +0800

    Fixed small bug in adding-image functionality.

---
+++ ConnectMe/ConnectMe/CameraController.swift
@@ -0,0 +1,13 @@
+//
+//  CameraController.swift
+//  ConnectMe
+//
+//  Created by Austin Vaday on 12/20/15.
+//  Copyright © 2015 ConnectMe. All rights reserved.
+//
+
+import UIKit
+
+class CameraController: UIViewController {
+
+}
---
+++ ConnectMe/ConnectMe/MenuController.swift
@@ -0,0 +1,13 @@
+//
+//  MenuController.swift
+//  ConnectMe
+//
+//  Created by Austin Vaday on 12/20/15.
+//  Copyright © 2015 ConnectMe. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+}
