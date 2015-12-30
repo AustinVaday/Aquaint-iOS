628775b42b4baa0e3090330ea953981bd1c96ab9
Austin Vaday <avaday0@ivc.edu>
2015-12-30 17:16:25 +0800

    Successfully added in threading to log user in while spinner plays. Now, trying to prevent user from clicking the log-in button more than once on a single request.

--- ConnectMe/ConnectMe/LogInController.swift
+++ ConnectMe/ConnectMe/LogInController.swift
@@ -15,9 +15,16 @@ class LogInController: UIViewController {
     @IBOutlet weak var userPassword: UITextField!
     @IBOutlet weak var checkMark: UIImageView!
     @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var logInButton: UIButton!
+    
+    
+    // Counts how many times the user has incorrectly logged in.
+    var wrongLogInCount: Int = 0
+    var alert: UIAlertController!
     
     override func viewDidLoad() {
         // Show activity indicator (spinner)
+        PFUser.logOut()
     }
     
     @IBAction func emailEditingDidEnd(sender: UITextField) {
@@ -28,59 +35,92 @@ class LogInController: UIViewController {
 
     @IBAction func loginButtonClicked(sender: UIButton) {
         
+        // Disable log in button so that user can only send one request at a time
+        logInButton.enabled = false
+        
         // Show activity indicator (spinner)
         spinner.startAnimating()
         
         let userEmailString:String = userEmail.text!
         let userPasswordString:String =  userPassword.text!
 
-        
-                
+        // Perform long-running operation on background thread
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            
 //        PFUser.logInWithUsernameInBackground("Austin", password: "123")
-        
-        do
-        {
+            do
+            {
+                
+                self.spinner.startAnimating()
             
+                try PFUser.logInWithUsername(userEmailString, password: userPasswordString)
+            
+                // Perform update on UI on main thread
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    
+                    // Stop showing activity indicator (spinner)
+                    self.spinner.stopAnimating()
+                    
+                    print("User logged in!")
+                    self.performSegueWithIdentifier("HomeViewController", sender: nil)
+                    
+                })
+          
 
-//            
-//            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-//                self.spinner.startAnimating()
-//            })
-            
-            try PFUser.logInWithUsername(userEmailString, password: userPasswordString)
-            
-            // Stop showing activity indicator (spinner)
-            spinner.stopAnimating()
-            
-            print("User logged in!")
-            performSegueWithIdentifier("HomeViewController", sender: nil)
             
             
-        }
-        // Catch exception and display error if user does not exist
-        catch
-        {
-            // Create alert to send to user
-            let alert = UIAlertController(title: "Please try again...", message: "The email and password do not match.", preferredStyle: UIAlertControllerStyle.Alert)
+            }
+            // Catch exception and display error if user does not exist
+            catch
+            {
+                // Perform update on UI on main thread
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    
+                    /*
+                    self.wrongLogInCount++
+                
+                    // If user logs in incorrectly more than three times, give them a chance to change password...
+                    if (self.wrongLogInCount > 3)
+                    {
+                        
+                    }
+                    */
+
+                        // Create alert to send to user
+                        self.alert = UIAlertController(title: "Please try again...", message: "The email and password do not match.", preferredStyle: UIAlertControllerStyle.Alert)
+                    
+                    
+                        // Create the action to add to alert
+                        let alertAction = UIAlertAction(title: "Try again", style: UIAlertActionStyle.Default, handler: nil)
+                        
+                        // Add the action to the alert
+                        self.alert.addAction(alertAction)
+                        
+                        // Stop showing activity indicator (spinner)
+                        self.spinner.stopAnimating()
+                    
+                    
+//                    print(self.alert.view.hidden)
+                    
+                    // Show the alert if it has not been showed already (we need this in case the user clicks many times -- quickly -- on the log-in button before it is disabled. This if statement prevents the display of multiple alerts).
+//                    if (self.alert.isViewLoaded && self.alert.view.window && self.alert.parentViewController != nil)
+//                    {
+                        self.showViewController(self.alert, sender: nil)
+
+//                    }
+                    
+                
+                print("LogIn Error")
+                
+                })
             
-            // Create the action to add to alert
-            let alertAction = UIAlertAction(title: "Try again", style: UIAlertActionStyle.Default, handler: nil)
+            }
             
-            // Add the action to the alert
-            alert.addAction(alertAction)
-            
-            // Stop showing activity indicator (spinner)
-            spinner.stopAnimating()
-            
-            // Show the alert
-            showViewController(alert, sender: nil)
-            
-            
-            print("LogIn Error")
-            
-        }
+        })
+        
+
+        // Enable the log-in button again
+        logInButton.enabled = true
         
     }
-    
-    
 }
