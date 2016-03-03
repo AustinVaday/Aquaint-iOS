2f3bafda7a6136342806748eb72c77f3faa17125
Austin Vaday <avaday0@ivc.edu>
2016-03-02 20:34:46 -0800

    Click on a button and it'll take you to my twitter page! Work in progress.

--- ConnectMe/ConnectMe/SocialMediaCollectionViewCell.swift
+++ ConnectMe/ConnectMe/SocialMediaCollectionViewCell.swift
@@ -16,6 +16,21 @@ class SocialMediaCollectionViewCell: UICollectionViewCell {
     @IBAction func emblemButtonClicked(sender: AnyObject) {
         
         print("The button was clicked!")
+        
+        var socialMediaURL = NSURL(string: "twitter:///user?screen_name=AustinVaday")
+//        var socialMediaURL = NSURL(string: "snapchat://?u=AustinVaday")
+//        var socialMediaURL = NSURL(string: "instagram://user?username=avtheman")
+
+        
+        // If user doesn't have social media app installed, open using default browser instead.
+        if(!UIApplication.sharedApplication().canOpenURL(socialMediaURL!))
+        {
+            socialMediaURL = NSURL(string: "http://www.twitter.com/AustinVaday")
+            
+        }
+ 
+        UIApplication.sharedApplication().openURL(socialMediaURL!)
+
 
     }
 }
