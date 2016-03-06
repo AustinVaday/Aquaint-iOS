f3b3ef200fa595575efedb6bbcac0f9220f3a640
Austin Vaday <avaday0@ivc.edu>
2016-03-05 19:50:40 -0800

    Attempted to add in video preview layer, failed. Will attempt again.

--- ConnectMe/ConnectMe/CameraController.swift
+++ ConnectMe/ConnectMe/CameraController.swift
@@ -14,19 +14,45 @@ import AVFoundation
 
 class CameraController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
+
+    @IBOutlet weak var previewView: UIView!
     var captureSession:AVCaptureSession!
     var videoPreviewLayer:AVCaptureVideoPreviewLayer!
     var scanCodeFrameView:UIView!
     
+    override func viewWillAppear(animated: Bool) {
+        
+//        //Create a capture session
+//        captureSession = AVCaptureSession()
+//        
+//        print ("TESTING0")
+//
+//        // Configure session - We need a capture session that captures a still image
+//        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+//        
+//        
+//        let rearCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+//        let input = try! AVCaptureDeviceInput(device: rearCamera)
+//        
+//        
+//        print ("TESTING")
+//        captureSession.addInput(input)
+//        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+//        previewView.layer.addSublayer(videoPreviewLayer)
+//
+//        print("TESTING 2")
+//        videoPreviewLayer.frame = previewView.bounds
+
+    }
+    
     override func viewDidLoad() {
         
-        // Create capture device object with the appropriate AVCaptureDevice we need for video processing
-        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
-        
+//        // Create capture device object with the appropriate AVCaptureDevice we need for video processing
+//        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+//        
 //        let input: AVCaptureInput = try! AVCaptureDeviceInput.init(device: captureDevice)
+
         
-        captureSession = AVCaptureSession()
-//        captureSession.addInput(input)
         
     }
     
