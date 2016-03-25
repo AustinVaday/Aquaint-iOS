02472f9abea88daeeb53486bbe97b5f1ff573711
Austin Vaday <avaday0@ivc.edu>
2016-03-25 13:47:09 -0700

    Added users to table, now I need to get the button to do something.

--- ConnectMe/ConnectMe/HomeController.swift
+++ ConnectMe/ConnectMe/HomeController.swift
@@ -20,9 +20,11 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
     var userName : String!
     var userId   : String!
     
-
+    
     let firebaseRootRefString = "https://torrid-fire-8382.firebaseio.com/"
     var firebaseRootRef : Firebase!
+    
+    var allUsers: Array<Connection>!
     
     
     override func viewDidLoad() {
@@ -38,7 +40,7 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
         {
             print("Error in HomeController. authData is somehow nil!")
             self.performSegueWithIdentifier("LogOut", sender: nil)
-        
+            
         }
         
         // Get current user from NSUserDefaults
@@ -47,7 +49,7 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
         if (userName != nil)
         {
             print(userName)
-
+            
             userNameLabel.text = userName
         }
         else
@@ -56,83 +58,124 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
         
         
-//        // Add gesture recognizer programatacially (buggy if doing so through XIB)
-//        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
-//        self.view.userInteractionEnabled = true
-//        self.view.addGestureRecognizer(panGestureRecognizer)
+        //        // Add gesture recognizer programatacially (buggy if doing so through XIB)
+        //        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
+        //        self.view.userInteractionEnabled = true
+        //        self.view.addGestureRecognizer(panGestureRecognizer)
+        
+        
+        // FOR FILLING THE TABLE:
+        
+        let firebaseUsersRef = Firebase(url: firebaseRootRefString + "Users/")
+        allUsers = Array<Connection>()
+        
+        firebaseUsersRef.observeEventType(FEventType.ChildAdded, withBlock: { (snapshot) -> Void in
+            
+            print(snapshot.value)
+            print("KEY IS: ", snapshot.key)
+            
+            let user = Connection()
+
+            // Store respective user info (key is the username)
+            user.userName = snapshot.key
+            
+            
+            // Retrieve user's other info
+            firebaseUsersRef.childByAppendingPath(user.userName).observeSingleEventOfType(FEventType.Value, withBlock: { (snapshot) -> Void in
+                
+                print(snapshot)
+                user.userFullName = snapshot.childSnapshotForPath("/fullName").value as! String
+                user.userImage    = snapshot.childSnapshotForPath("/userImage").value as! String
+                
+                self.allUsers.append(user)
+                
+                self.searchTableView.reloadData()
+                
+                print("RELOADED")
+                
+            })
+            
+        })
+        
+        
         
     }
-/*
+    /*
     // Functionality to handle user pan gestures (dragging left, right, up, down, etc)
     func handlePan (recognizer: UIPanGestureRecognizer)
     {
-        print("IN HANDLEPAN")
-        // Get the translation (how much the user moved their finger)
-        let translation = recognizer.translationInView(self.view)
-        let velocity = recognizer.velocityInView(self.view)
-        let view = recognizer.view!
-        
-        // Set the new view's center based on x/y translations that the user initiated
-        // No y translation for now
-        view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y /*+ translation.y*/)
+    print("IN HANDLEPAN")
+    // Get the translation (how much the user moved their finger)
+    let translation = recognizer.translationInView(self.view)
+    let velocity = recognizer.velocityInView(self.view)
+    let view = recognizer.view!
     
-        // Make sure to set recognizer's translation back to 0 to prevent compounding issues
-        recognizer.setTranslation(CGPointZero, inView: self.view)
-
-
+    // Set the new view's center based on x/y translations that the user initiated
+    // No y translation for now
+    view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y /*+ translation.y*/)
+    
+    // Make sure to set recognizer's translation back to 0 to prevent compounding issues
+    recognizer.setTranslation(CGPointZero, inView: self.view)
+    
+    
     }
-*/
+    */
     @IBAction func menuButtonClicked(sender: AnyObject) {
         // Transition to page on left (menu)
         
-//        let pageViewController = storyboard?.instantiateViewControllerWithIdentifier("MainPageViewController") as! MainPageViewController
-//        
-//        
-//        let menuViewController = storyboard?.instantiateViewControllerWithIdentifier("MenuViewController") as! MenuController
-//        
-//        pageViewController.setViewControllers([menuViewController], direction: UIPageViewControllerNavigationDirection.Reverse, animated: true) { (Bool) -> Void in
-//            print("setviewControllers finished")
-//        }
-//        
-//        print("TESTING OK")
-//        menuViewController.testLabel.text = "TESTING TESTING"
-//        
-//        
-//        
+        //        let pageViewController = storyboard?.instantiateViewControllerWithIdentifier("MainPageViewController") as! MainPageViewController
+        //
+        //
+        //        let menuViewController = storyboard?.instantiateViewControllerWithIdentifier("MenuViewController") as! MenuController
+        //
+        //        pageViewController.setViewControllers([menuViewController], direction: UIPageViewControllerNavigationDirection.Reverse, animated: true) { (Bool) -> Void in
+        //            print("setviewControllers finished")
+        //        }
+        //
+        //        print("TESTING OK")
+        //        menuViewController.testLabel.text = "TESTING TESTING"
+        //
+        //
+        //
+        
+        self.searchTableView.reloadData()
+        
+        print("RELOADED")
+        
         
         
     }
     @IBAction func logOutButtonClicked(sender: UIButton) {
         
-            // Ask user if they really want to log out...
-            let alert = UIAlertController(title: nil, message: "Are you really sure you want to log out?", preferredStyle: UIAlertControllerStyle.Alert)
+        // Ask user if they really want to log out...
+        let alert = UIAlertController(title: nil, message: "Are you really sure you want to log out?", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let logOutAction = UIAlertAction(title: "Log out", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
             
-            let logOutAction = UIAlertAction(title: "Log out", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
+            // present the log in home page
             
-                // present the log in home page
+            //TODO: Add spinner functionality
+            self.performSegueWithIdentifier("LogOut", sender: nil)
+            
+            // Log out of of firebase
+            self.firebaseRootRef.unauth()
+            
+            // Remove all observers
+            self.firebaseRootRef.removeAllObservers()
+            
+            if (self.firebaseRootRef.authData == nil)
+            {
+                print("successful log out.")
                 
-                //TODO: Add spinner functionality
-                self.performSegueWithIdentifier("LogOut", sender: nil)
-                
-                // Log out of of firebase
-                self.firebaseRootRef.unauth()
-                
-                // Remove all observers
-                self.firebaseRootRef.removeAllObservers()
-                
-                if (self.firebaseRootRef.authData == nil)
-                {
-                    print("successful log out.")
-                    
-                    // Set initial view controller back to default
-//
-//                    let window = UIWindow(frame: UIScreen.mainScreen().bounds)
-//                    let storyboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
-//                    let viewControllerIdentifier = "BeginningViewController"
-//                    
-//                    // Go to beginning page, as if user was logged in already!
-//                    window.rootViewController = storyboard.instantiateViewControllerWithIdentifier(viewControllerIdentifier)
-                }
+                // Set initial view controller back to default
+                //
+                //                    let window = UIWindow(frame: UIScreen.mainScreen().bounds)
+                //                    let storyboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+                //                    let viewControllerIdentifier = "BeginningViewController"
+                //
+                //                    // Go to beginning page, as if user was logged in already!
+                //                    window.rootViewController = storyboard.instantiateViewControllerWithIdentifier(viewControllerIdentifier)
+            }
         }
         
         let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: nil)
@@ -151,7 +194,11 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         let cell = tableView.dequeueReusableCellWithIdentifier("searchCell", forIndexPath: indexPath) as! SearchTableViewCell
         
-        cell.cellName.text = "HEY"
+        let userFullName = allUsers[indexPath.item].userFullName
+        let userName     = allUsers[indexPath.item].userName
+        let userImage    = allUsers[indexPath.item].userImage
+        
+        cell.cellName.text = userFullName + " (" + userName + ")"
         
         return cell
         
@@ -159,7 +206,7 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 20
+        return allUsers.count
         
     }
     
