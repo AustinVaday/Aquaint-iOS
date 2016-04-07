c932a5f4958e69a0da4d0ebe51bd95318a27ba6c
Austin Vaday <avaday0@ivc.edu>
2016-04-07 12:03:19 -0700

    Moved table of all users to search view controller.

--- ConnectMe/ConnectMe/HomeController.swift
+++ ConnectMe/ConnectMe/HomeController.swift
@@ -12,11 +12,11 @@ import UIKit
 import Firebase
 import AWSS3
 
-class HomeController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class HomeController: UIViewController{
     
     
     @IBOutlet weak var userNameLabel: UILabel!
-    @IBOutlet weak var searchTableView: UITableView!
+//    @IBOutlet weak var searchTableView: UITableView!
     
 //    @IBOutlet weak var notificationViewLabel: UILabel!
 //    @IBOutlet weak var notificationView: UIView!
@@ -35,7 +35,7 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
     var firebaseRootRef : Firebase!
     let awsBucketName = "aquaint-userimages"
     
-    var allUsers: Array<Connection>!
+//    var allUsers: Array<Connection>!
     
     var connectionRequestList : Array<String>! // MAKE IT Connection type LATER
     
@@ -215,36 +215,36 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         let firebaseUsersRef = Firebase(url: firebaseRootRefString + "Users/")
 
-        allUsers = Array<Connection>()
+//        allUsers = Array<Connection>()
         connectionRequestList = Array<String>()
         
-        firebaseUsersRef.observeEventType(FEventType.ChildAdded, withBlock: { (snapshot) -> Void in
-            
-            print(snapshot.value)
-            print("KEY IS: ", snapshot.key)
-            
-            let user = Connection()
-
-            // Store respective user info (key is the username)
-            user.userName = snapshot.key
-            
-            
-            // Retrieve user's other info
-            firebaseUsersRef.childByAppendingPath(user.userName).observeSingleEventOfType(FEventType.Value, withBlock: { (snapshot) -> Void in
-                
-                print(snapshot)
-                user.userFullName = snapshot.childSnapshotForPath("/fullName").value as! String
-                user.userImage    = snapshot.childSnapshotForPath("/userImage").value as! String
-                
-                self.allUsers.append(user)
-                
-                self.searchTableView.reloadData()
-                
-                print("RELOADED")
-                
-            })
-            
-        })
+//        firebaseUsersRef.observeEventType(FEventType.ChildAdded, withBlock: { (snapshot) -> Void in
+//            
+//            print(snapshot.value)
+//            print("KEY IS: ", snapshot.key)
+//            
+//            let user = Connection()
+//
+//            // Store respective user info (key is the username)
+//            user.userName = snapshot.key
+//            
+//            
+//            // Retrieve user's other info
+//            firebaseUsersRef.childByAppendingPath(user.userName).observeSingleEventOfType(FEventType.Value, withBlock: { (snapshot) -> Void in
+//                
+//                print(snapshot)
+//                user.userFullName = snapshot.childSnapshotForPath("/fullName").value as! String
+//                user.userImage    = snapshot.childSnapshotForPath("/userImage").value as! String
+//                
+//                self.allUsers.append(user)
+//                
+//                self.searchTableView.reloadData()
+//                
+//                print("RELOADED")
+//                
+//            })
+//            
+//        })
         
         
         
@@ -287,7 +287,7 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
         //
         //
         
-        self.searchTableView.reloadData()
+//        self.searchTableView.reloadData()
         
         print("RELOADED")
         
@@ -345,31 +345,30 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
         
     }
     
-    
-    // **** SEARCH TABLE VIEW *****
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCellWithIdentifier("searchCell", forIndexPath: indexPath) as! SearchTableViewCell
-        
-        let userFullName = allUsers[indexPath.item].userFullName
-        let userName     = allUsers[indexPath.item].userName
-        let userImage    = allUsers[indexPath.item].userImage
-        
-        cell.cellName.text = userFullName
-        cell.cellUserName.text = userName
-        
-        return cell
-        
+//    
+//    // **** SEARCH TABLE VIEW *****
+//    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        
+//        let cell = tableView.dequeueReusableCellWithIdentifier("searchCell", forIndexPath: indexPath) as! SearchTableViewCell
+//        
+//        let userFullName = allUsers[indexPath.item].userFullName
+//        let userName     = allUsers[indexPath.item].userName
+//        let userImage    = allUsers[indexPath.item].userImage
+//        
+//        cell.cellName.text = userFullName
+//        cell.cellUserName.text = userName
+//        
+//        return cell
+//        
+//    }
+//    
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        
+//        return allUsers.count
+//        
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return allUsers.count
-        
-    }
     
     
-    
-    
-}
+
--- ConnectMe/ConnectMe/SearchViewController.swift
+++ ConnectMe/ConnectMe/SearchViewController.swift
@@ -7,7 +7,88 @@
 //
 
 import UIKit
+import Firebase
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var searchTableView: UITableView!
+    
+    var userName : String!
+    var userId   : String!
+    var firebaseRootRef : Firebase!
+    var allUsers: Array<Connection>!
+    
+    
+    let firebaseRootRefString = "https://torrid-fire-8382.firebaseio.com/"
+    
+
+
+    override func viewDidLoad(){
+        
+//        userName = getCurrentUser()
+        
+//        firebaseRootRef = Firebase(url: firebaseRootRefString)
+
+        let firebaseUsersRef = Firebase(url: firebaseRootRefString + "Users/")
+        
+        allUsers = Array<Connection>()
+        
+        firebaseUsersRef.observeEventType(FEventType.ChildAdded, withBlock: { (snapshot) -> Void in
+            
+            print(snapshot.value)
+            print("KEY IS: ", snapshot.key)
+            
+            let user = Connection()
+            
+            // Store respective user info (key is the username)
+            user.userName = snapshot.key
+            
+            
+            // Retrieve user's other info
+            firebaseUsersRef.childByAppendingPath(user.userName).observeSingleEventOfType(FEventType.Value, withBlock: { (snapshot) -> Void in
+                
+                print(snapshot)
+                user.userFullName = snapshot.childSnapshotForPath("/fullName").value as! String
+                user.userImage    = snapshot.childSnapshotForPath("/userImage").value as! String
+                
+                self.allUsers.append(user)
+                
+                self.searchTableView.reloadData()
+                
+                print("RELOADED")
+                
+            })
+            
+        })
+
+        
+        
+
+        
+    }
+    
+    // **** SEARCH TABLE VIEW *****
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("searchCell", forIndexPath: indexPath) as! SearchTableViewCell
+        
+        let userFullName = allUsers[indexPath.item].userFullName
+        let userName     = allUsers[indexPath.item].userName
+        let userImage    = allUsers[indexPath.item].userImage
+        
+        cell.cellName.text = userFullName
+        cell.cellUserName.text = userName
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return allUsers.count
+        
+    }
+
 
 }
