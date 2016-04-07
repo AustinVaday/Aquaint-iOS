2afee95cc34525029a37d9d5d8adc31523cfee17
Austin Vaday <avaday0@ivc.edu>
2016-04-07 11:36:58 -0700

    Added in new view controllers to support the footer.

---
+++ ConnectMe/ConnectMe/SearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.swift
+//  
+//
+//  Created by Austin Vaday on 4/7/16.
+//
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+}
---
+++ ConnectMe/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  Aquaint
+//
+//  Created by Austin Vaday on 4/7/16.
+//  Copyright © 2016 ConnectMe. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+}
