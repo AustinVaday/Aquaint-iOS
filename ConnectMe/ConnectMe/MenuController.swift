359be6f53eb1fa6bd252ce10b66fd0ec9e4e3f6c
Austin Vaday <avaday0@ivc.edu>
2016-04-09 23:49:42 -0700

    Updated menu page. Changed design a bit.

--- ConnectMe/ConnectMe/MenuController.swift
+++ ConnectMe/ConnectMe/MenuController.swift
@@ -10,7 +10,58 @@
 
 import UIKit
 
-class MenuController: UIViewController {
+class MenuController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var testLabel: UILabel!
 
+    
+    override func viewDidLoad() {
+        
+        
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("menuCell") as! MenuTableViewCell!
+        
+        switch (indexPath.row)
+        {
+        case 0:
+            cell.cellName.text = "Your Account"
+            break;
+        case 1:
+            cell.cellName.text = "Linked Social Media Accounts"
+            break;
+        case 2:
+            cell.cellName.text = "Notification Settings"
+            break;
+        case 3:
+            cell.cellName.text = "Invite Friends"
+            break;
+        case 4:
+            cell.cellName.text = "Help & About Us"
+            break;
+        case 5:
+            cell.cellName.text = "Log Out"
+            break;
+    
+        default:
+            cell.cellName.text = "Error"
+        
+        }
+        
+
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 }
---
+++ ConnectMe/ConnectMe/MenuTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MenuTableViewCell.swift
+//  Aquaint
+//
+//  Created by Austin Vaday on 4/9/16.
+//  Copyright © 2016 ConnectMe. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellName: UILabel!
+    @IBOutlet weak var cellImage: UIImageView!
+}
