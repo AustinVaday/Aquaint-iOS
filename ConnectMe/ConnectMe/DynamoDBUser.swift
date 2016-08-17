cbef5b595a432f49a5ac9de7d169e74ab2e01cc9
Austin Vaday <avaday0@ivc.edu>
2016-08-16 20:29:47 -0700

    Beginning object oriented design for the Newsfeed.

--- ConnectMe/ConnectMe/DynamoDBUser.swift
+++ ConnectMe/ConnectMe/DynamoDBUser.swift
@@ -29,3 +29,48 @@ class User : AWSDynamoDBObjectModel
     }
     
 }
+
+
+class NewsfeedObject
+{
+    var username : String!
+    var event : String!
+    var otherUser : String!
+    var timestamp : String!
+}
+
+
+class NewsfeedObjectModel : AWSDynamoDBObjectModel
+{
+
+    var newsfeedList : Array<NewsfeedObject>!
+    let maxSize = 10 // Denotes how much data to store for one user's newsfeed
+    
+    override init()
+    {
+        super.init()
+        
+        newsfeedList = Array<NewsfeedObject>()
+        
+    }
+    
+    required init!(coder: NSCoder!) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    class func dynamoDBTableName() -> String {
+        
+        return "aquaint-newsfeed"
+    }
+    
+    class func hashKeyAttribute() -> String {
+        
+        return "username"
+    }
+    
+    func addNewsfeedObject(object: NewsfeedObject)
+    {
+        
+    }
+    
+}
