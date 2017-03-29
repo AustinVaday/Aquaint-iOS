ab343c52854763e6f10cd1210fa7e0bb556c6b01
Austin Vaday <avaday0@ivc.edu>
2017-03-28 18:16:22 -0700

    Deprecating Following/Followers pages. Adding in analytics and payments

---
+++ ConnectMe/ConnectMe/PaymentsDisplay.swift
@@ -0,0 +1,22 @@
+//
+//  PaymentsDisplay.swift
+//  Aquaint
+//
+//  Created by Austin Vaday on 3/28/17.
+//  Copyright © 2017 ConnectMe. All rights reserved.
+//
+
+import UIKit
+
+// Will have the real displays and data
+class PaymentsDisplay: UIViewController {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ConnectMe/ConnectMe/PaymentsDisplayViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PaymentsDisplayViewController.swift
+//  Aquaint
+//
+//  Created by Austin Vaday on 3/28/17.
+//  Copyright © 2017 ConnectMe. All rights reserved.
+//
+
+import UIKit
+
+class PaymentsDisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
