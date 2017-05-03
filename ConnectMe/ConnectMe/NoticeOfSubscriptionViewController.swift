7609bfeca503ba27676f65712eeaaf60b58a13c8
Yingbo (Max) Wang <maxwyb@gmail.com>
2017-05-03 00:31:28 -0700

    Add Notice of Purchase view controller.

---
+++ ConnectMe/ConnectMe/NoticeOfSubscriptionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NoticeOfSubscriptionViewController.swift
+//  Aquaint
+//
+//  Created by Yingbo Wang on 5/2/17.
+//  Copyright © 2017 ConnectMe. All rights reserved.
+//
+
+import Foundation
+
+class NoticeOfSubscriptionViewController: UIViewController {
+  @IBAction func doneButtonClicked(sender: UIButton) {
+    self.dismissViewControllerAnimated(true, completion: nil)
+  }
+}
