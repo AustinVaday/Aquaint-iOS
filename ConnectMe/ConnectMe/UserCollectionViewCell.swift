a2d9561e3e0226ce953248cb06af64c1c91a2f05
Yingbo (Max) Wang <maxwyb@gmail.com>
2017-05-28 20:36:19 -0700

    Code inspection. Add CollectionView cell's custom class file.

---
+++ ConnectMe/ConnectMe/UserCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  UserCollectionViewCell.swift
+//  Aquaint
+//
+//  Created by Yingbo Wang on 5/28/17.
+//  Copyright © 2017 ConnectMe. All rights reserved.
+//
+
+import UIKit
+
+class UserCollectionViewCell: UICollectionViewCell {
+  
+  @IBOutlet weak var userProfileImage: UIImageView!
+  
+  @IBOutlet weak var usernameLabel: UILabel!
+}
